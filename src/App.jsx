d02bc361ecad4b54981d7f20b04b824fef6e192f
PColares <siqueira.pedro1998@gmail.com>
2022-07-13 09:43:36 -0300

    Add Initial project

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Post from './Post'
+
+function App() {
+  return (
+    <>
+      <Post
+        author="Pedro Colares"
+        content="Qui dolor cillum minim non incididunt anim eu dolor nisi qui cupidatat est." />
+    </>
+  )
+}
+
+export default App
